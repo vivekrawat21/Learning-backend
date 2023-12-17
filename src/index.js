7ce4861ae0a717f7049a5895cd38a7fe0cc99b9f
vivekrawat21 <vivekrwt2111@gmail.com>
2023-12-17 17:12:18 +0530

    connect to monogatlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config({path:'./env'}); it is lacking in code consistency
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./env' //This is the better approach for code 
+});
+connectDB();
+
+
+
+
+
+
+
+
+
+/* -----FIRST APPOACH FOR DATABASE CONNECTION---------
+import express from "express";
+//Wrap the database connection into try catch or promises 
+//Database is always in another continent so time lagega and we have to use async await
+
+const app = express();
+
+
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`); //we have to give the database name and the url of the database
+        app.on("error",(error)=>{
+            console.log("ERR: ",error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log("App is listening on port:  ",process.env.PORT);
+        })
+    }
+    catch(error){
+        console.error(error);
+        throw error
+    }
+})(); //ifise is  used to execute the function immediately*/
