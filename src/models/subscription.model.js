aa6bf8efb9c30e018ed02c99f1c144d8f2e0ed43
vivekrawat21 <vivekrwt2111@gmail.com>
2024-01-14 22:55:21 +0530

    some comments

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -21,3 +21,11 @@ const subscriptionSchema = new Schema(
 );
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
+//subscription schema :
+//How it actually works
+//Contains two major things 
+// 1. Subscribers  2.Channels
+//when the code run in every  a new document will created
+//So we can say that everyone can subcribe to different channels and many channels can subscribe to same channel
+//For getting the subscriber count we will count the document which have channel of the youtube name so we count the documnet which have the channel name 
+// when we want to check which channel do i have subscribed for that we  selected the document where subscriber value is i and get the channel name for my every document .
